3d677f67e762f4d276410a190cd02ec66cd3c4a4
maenttang <36221352+maenttang@users.noreply.github.com>
2018-11-18 17:35:53 +0900

    commit

--- src/practice01/Prob4.java
+++ src/practice01/Prob4.java
@@ -11,6 +11,19 @@ public class Prob4 {
 		String s = scanner.nextLine();
 		
 		/* 코드를 완성합니다. */
+		int size = s.length();
+		for(int  i=0; i<size; i++)
+		{
+			//System.out.println(s);
+			for(int j = 0; j< 2; j++)
+			{
+				System.out.print(s.charAt(j));
+			}
+			System.out.println("");
+		}
+		
+		
+		
 		System.out.println(s);
 		
 		scanner.close();
